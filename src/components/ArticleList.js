aecddb0dadfe26715645db1962b89b07f8b08e80
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-11-03 17:49:16 -0400

    Completed Assignmetn

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,11 @@ import Article from "./Article"
 
 function ArticleList({posts}){
     const articleListArray = posts.map(post => {
-        return <Article key= {post.id} title = {post.title} date = {post.date} /> 
+        return <Article 
+        key= {post.id} 
+        title = {post.title} 
+        date = {post.date}
+        preview = {post.preview} /> 
     })  
     return (
         <main>
